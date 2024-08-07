7e1c0acc426f2945313eb4307e6b4d3095a6b223
Jaipur-rugs <abhishek.pa@pixxeldigital.com>
2024-08-07 17:37:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/CustomerData', {
